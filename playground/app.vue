c63c5976fb0ecbe108e47af5a18ee439e74b1845
anhtv56 <anhtv56@ghtk.co>
2022-10-08 09:50:37 +0700

    update it

--- playground/app.vue
+++ playground/app.vue
@@ -3,10 +3,13 @@
 
 <template>
   <!-- Edit this file to play around with Nuxt but never commit changes! -->
-  <div>
+  <div class="hello">
     Nuxt 3 Playground
   </div>
 </template>
 
 <style scoped>
+  .hello {
+    color: gray;
+  }
 </style>
